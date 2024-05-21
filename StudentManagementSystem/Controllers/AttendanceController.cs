865dbdc991c16b90a116cbe3f949f400f9f2cdb6
yeyintj <yeyinttttt2@gmail.com>
2024-05-21 22:59:54 +0630

    added after new

---
+++ StudentManagementSystem/Controllers/AttendanceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        public IActionResult Entry()
+        {
+            return View();
+        }
+    }
+}
---
+++ StudentManagementSystem/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Entry()
+        {
+            return View();
+        }
+    }
+}
