a8dca86d8258b84aa6906c3ddba91fce79b2fc27
yeyintj <yeyinttttt2@gmail.com>
2024-06-02 13:20:51 +0630

    added changes

--- StudentManagementSystem/Controllers/AssignmentController.cs
+++ StudentManagementSystem/Controllers/AssignmentController.cs
@@ -35,10 +35,6 @@ namespace StudentManagementSystem.Controllers
             var batches = (from batch in _dbContext.Batches
                            join course in _dbContext.Courses
                            on batch.CourseId equals course.Id
-<<<<<<< HEAD
-=======
-
->>>>>>> yairyint
                            select new BatchViewModel
                            {
                                Id = batch.Id,
